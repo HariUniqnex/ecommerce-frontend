229bbddd21a294e6a386cca8dfa3a39cd2ff19b3
Harisankar <harisankar@uniqnex360.com>
2025-06-26 13:14:39 +0530

    static json added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import OrdersList from './components/OrdersList';
 import Dashboard from './components/Dashboard';
 import { CssBaseline, Container, ThemeProvider, createTheme } from '@mui/material';
